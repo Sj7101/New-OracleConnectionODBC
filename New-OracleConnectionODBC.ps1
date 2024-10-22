bd60dbc9e35fdfbd041ff554352f89aa1e2066b5
Sj7101 <32309663+Sj7101@users.noreply.github.com>
2024-10-22 10:49:49 -0500

    Update New-OracleConnectionODBC.ps1

--- New-OracleConnectionODBC.ps1
+++ New-OracleConnectionODBC.ps1
@@ -42,3 +42,28 @@ try {
 
 # Clear the plain-text password from memory
 [Runtime.InteropServices.Marshal]::ZeroFreeBSTR([Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePassword))
+
+
+<#
+
+$releaseKey = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" -ErrorAction Stop).Release
+switch ($releaseKey) {
+    { $_ -ge 533325 } { ".NET Framework 4.8.1 or later"; break }
+    { $_ -ge 528040 } { ".NET Framework 4.8"; break }
+    { $_ -ge 461814 } { ".NET Framework 4.7.2"; break }
+    { $_ -ge 461808 } { ".NET Framework 4.7.2 (Windows 10 April 2018 Update)"; break }
+    { $_ -ge 461308 } { ".NET Framework 4.7.1"; break }
+    { $_ -ge 460798 } { ".NET Framework 4.7"; break }
+    { $_ -ge 394802 } { ".NET Framework 4.6.2"; break }
+    { $_ -ge 394254 } { ".NET Framework 4.6.1"; break }
+    { $_ -ge 393295 } { ".NET Framework 4.6"; break }
+    { $_ -ge 379893 } { ".NET Framework 4.5.2"; break }
+    { $_ -ge 378675 } { ".NET Framework 4.5.1"; break }
+    { $_ -ge 378389 } { ".NET Framework 4.5"; break }
+    default { "Version 4.5 or earlier"; break }
+}
+
+
+
+
+#>
